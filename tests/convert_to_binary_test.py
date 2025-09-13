8d95ee1ee07fb961d98a3fc45bb559c7cf50d7e5
Okerew <93822247+Okerew@users.noreply.github.com>
2025-09-13 11:00:11 +0200

    Delete tests directory

--- tests/convert_to_binary_test.py
+++
@@ -1,5 +0,0 @@
-from biobridge.utilities.convert_to_binary import BinaryConverter
-
-converter = BinaryConverter("Hello123World45")
-binary_output = converter.convert_string_with_ints_to_binary()
-print(binary_output)
--- tests/detect_landmarks_test.py
+++
@@ -1,9 +0,0 @@
-from biobridge.tools.bpa import BodyPartAnalyzer
-
-
-def test_detect_landmarks():
-    analyzer = BodyPartAnalyzer()
-    analyzer.detect_landmarks('image_examples/head.jpg')
-
-
-test_detect_landmarks()
